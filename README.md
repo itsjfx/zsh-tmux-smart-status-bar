cdd11db9d015fc0511c3ffd6ce448b76028eac7f
Thomas <me@jfx.ac>
2023-01-10 22:56:26 +1100

    add in a bunch of stuff

---
+++ README.md
@@ -0,0 +1,32 @@
+# zsh-tmux-smart-status-bar
+
+## Overview
+
+I installed `tmux` for the status bar so I could have a clean prompt.
+
+My goal was to have information on the current branch, AWS environment variables -- and to have meaningful window/tab names about the currently executed command or opened directory. 
+
+There's a few scripts around, but they didn't fulfill my requirements:
+* not great update mechanisms (e.g. polling, or on window switching)
+* some were overly heavy and added delay to my shell
+* just overly complicated in general, this is a status bar and it just needs to run a shell command and dump some environment variables
+* missing something from the second sentence above
+
+Some existing projects:
+* <https://github.com/arl/gitmux>
+* <https://github.com/drmad/tmux-git>
+* <https://github.com/mbenford/zsh-tmux-auto-title>
+* <https://github.com/ofirgall/tmux-window-name>
+
+The reason why I call this "smart" is cause just like [zsh-tmux-auto-title](https://github.com/mbenford/zsh-tmux-auto-title), it uses [terminal escape sequences](https://en.wikipedia.org/wiki/ANSI_escape_code) to update the status bar using [zsh hooks](https://zsh.sourceforge.io/Doc/Release/Functions.html).
+
+This means that it'll update the status bar in the most efficient manner (not spawning `tmux` processes), and it'll happen in real time as changes are made (when the shell prompt is rendered, or when a command is executed).
+
+I use terminal escape sequences to modify the window name and pane name, where I make the pane name the content of the right hand side of my status bar. This is where my "meta" status info lives (e.g. current `git` branch, etc). I'm not using multiple panes, so this seems ok at the moment -- but if I face any issues I'll likely have to set the status text directly within the script calling `tmux`.
+
+Things to do:
+* make the window names even better or selective around what args are listed, e.g. like [tmux-window-name](https://github.com/ofirgall/tmux-window-name)
+
+## Installation
+
+Just source this in your `.zshrc` :)
---
+++ zsh-tmux-smart-status-bar.sh
@@ -0,0 +1,59 @@
+# set ft=zsh
+
+# TODO may remove this cause you can do trimming and padding natively in tmux
+# https://github.com/tmux/tmux/wiki/Formats#trimming-and-padding
+_TMUX_WINDOW_NAME_MAX_LEN=25
+
+# inspired by https://github.com/mbenford/zsh-tmux-auto-title/blob/07fd6d7864df9aed4fbc5e1f67e1ad6eeef0a01f/zsh-tmux-auto-title.plugin.zsh#L17-L22
+_tmux_smart_title_set_title() {
+    case "$1" in
+		window) printf "\ek%s\e\\" "${2:0:"$_TMUX_WINDOW_NAME_MAX_LEN"}" ;;
+		pane)   printf "\e]2;%s\e\\" "$2" ;;
+	esac
+}
+
+_tmux_window_name_preexec_hook() {
+    local output="$2"
+    _tmux_smart_title_set_title window "$output"
+}
+
+# TODO to use ${PWD##*/} or not to use
+# TODO add some smarts to only show compact args for certain commands like: git, nvim, etc, and command name for rest
+_tmux_window_name_precmd_hook() {
+    local output="$(basename "$PWD")"
+    _tmux_smart_title_set_title window "$output"
+}
+
+# example of another way using tmux command directly: https://github.com/drmad/tmux-git/blob/master/tmux-git.sh
+# seems to modify global?
+# abusing pane-title terminal escape sequence to rename for current window only: https://github.com/tmux/tmux/wiki/Advanced-Use#pane-titles-and-the-terminal-title
+# cause I use a single pane
+# where status-right is just "#{pane_title}"
+# means escape sequences are used which is lighter
+_tmux_status_bar_preexec_hook() {
+    local gitref="$(timeout 0.05 sh -c 'git symbolic-ref -q --short HEAD || git describe HEAD --always --tags' 2>/dev/null)"
+    local output=()
+    if [ -n "$AWS_PROFILE" ]; then
+        output+=(": $AWS_PROFILE ${AWS_REGION:-$AWS_DEFAULT_REGION}") # not sure if needed
+    fi
+#    if [ -n "$FNM_DIR" ]; then
+#        output+=(": $(fnm current)")
+#    fi
+    # TODO
+    if [[ "$LD_PRELOAD" == *libproxychains4.so* ]]; then
+        output+=("PROXYCHAINS ON")
+    fi
+    if [ -n "$gitref" ]; then
+        output+=(" branch: $gitref") # small icon
+    fi
+    _tmux_smart_title_set_title pane "${(j: | :)output}"
+}
+
+# https://zsh.sourceforge.io/Doc/Release/Functions.html
+autoload -U add-zsh-hook
+# chpwd?
+# before prompt
+add-zsh-hook precmd _tmux_window_name_precmd_hook
+add-zsh-hook precmd _tmux_status_bar_preexec_hook
+# before executed command
+add-zsh-hook preexec _tmux_window_name_preexec_hook
